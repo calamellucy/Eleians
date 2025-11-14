c16a8d8a11ee1a2f2e376040123c9435876b15cd
calamellucy <sua050526@naver.com>
2025-11-15 03:44:11 +0900

    monster bug modify
    몬스터 여러가지 버그 수정, 몬스터 코드 구조 몬스터 추가에 유리하도록 수정, Monster base 추가

--- Assets/Eleians/Code/main_code/TowerMonster.cs
+++ Assets/Eleians/Code/main_code/TowerMonster.cs
@@ -2,33 +2,28 @@ using UnityEngine;
 
 public class TowerMonster : NormalMonster
 {
-    // NormalMonster의 OnEnable() 대신 Tower를 타겟으로 지정
     protected override void OnEnable()
     {
-        base.OnEnable(); // NormalMonster의 초기화 과정 유지
+        base.OnEnable();
 
-        // target을 Tower로 변경
-        if (GameManager.instance.tower != null)
-            target = GameManager.instance.tower.GetComponent<Rigidbody2D>();
+        target = GameManager.instance.tower.GetComponent<Rigidbody2D>();
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    // Tower만 공격
+    protected override void OnHitTower(Tower tower)
     {
-        if (!isLive) return;
+        attackTimer -= Time.deltaTime;
 
-        if (collision.CompareTag("Tower"))
+        if (attackTimer <= 0f)
         {
-            Tower tower = collision.GetComponent<Tower>();
-            if (tower != null)
-            {
-                Debug.Log("거점 타격 성공");
-                tower.TakeDamage(10); // 데미지 값은 조정 가능
-                isLive = false;
-                coll.enabled = false;
-                rigid.simulated = false;
-                anim.SetBool("dead", true);
-                GameManager.instance.kill++;
-            }
+            tower.TakeDamage(damage);
+            attackTimer = attackDelay;
         }
+    }
+
+    // Player는 무시하도록 비워두기
+    protected override void OnHitPlayer(Player player)
+    {
+        // 아무것도 안 함
     }
 }
