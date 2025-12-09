07b684e57e9356bb8bdee38cd2bdce5d36b137d6
calamellucy <sua050526@naver.com>
2025-12-09 13:14:02 +0900

    도전과제 수정

--- Assets/Eleians/Code/main_code/GameManager.cs
+++ Assets/Eleians/Code/main_code/GameManager.cs
@@ -238,6 +238,7 @@ public class GameManager : MonoBehaviour
                     }
                 }
             }
+            /*
             else
             {
                 BossMonster bm = boss.GetComponent<BossMonster>();
@@ -249,6 +250,7 @@ public class GameManager : MonoBehaviour
                     AchievementManager.instance.OnBossKilled();
                 }
             }
+            */
 
         }
         else
@@ -1146,6 +1148,12 @@ public class GameManager : MonoBehaviour
     {
         // 이미 엔딩 진행 중이면 무시
         if (!isLive) return;
+
+        // ★★★ [수정 핵심] isLive를 끄기 전에 업적 달성 체크를 먼저 해야 함! ★★★
+        if (AchievementManager.instance != null)
+        {
+            AchievementManager.instance.OnBossKilled();
+        }
 
         isLive = false; // 게임 로직 정지 (시간은 흐름)
         StartCoroutine(CoGameClearSequence());
